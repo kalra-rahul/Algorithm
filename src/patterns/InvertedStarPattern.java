d6ee1ffa9e78868ee19f104970951bd66c1d8933
kalra-rahul <kalrarahul431@gmail.com>
2026-02-01 02:48:53 +0530

    Patterns DSA

---
+++ src/patterns/InvertedStarPattern.java
@@ -0,0 +1,15 @@
+package patterns;
+
+public class InvertedStarPattern {
+    public static void main (String[] args){
+        int size = 5;
+        for(int i=0; i<size; i++){
+            for(int j=0; j<(size-i); j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
